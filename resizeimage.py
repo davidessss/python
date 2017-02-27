9fcc13c4ae5f6c154644b6662b5dc80d531a043f
davidessss <dvdgsxr@gmail.com>
2017-02-27 16:56:38 +0100

    Update resizeimage.py

--- resizeimage.py
+++ resizeimage.py
@@ -65,6 +65,47 @@ def file_size(file_path):
 		file_info = os.stat(file_path)
 		return convert_bytes(file_info.st_size)
 
+from PIL import Image
+import os
+import errno
+import shutil
+import glob
+import sys
+import logging
+
+def make_sure_path_exists(path):
+	try:
+		os.makedirs(path)
+	except OSError as exception:
+		if exception.errno != errno.EEXIST:
+			raise
+def convert_bytes(num):
+	"""
+	this function will convert bytes to MB.... GB... etc
+	"""
+	for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+		if num < 1024.0:
+			return "%3.1f %s" % (num, x)
+		num /= 1024.0
+		
+def getFolderSize(folder):
+	total_size = os.path.getsize(folder)
+	for item in os.listdir(folder):
+		itempath = os.path.join(folder, item)
+		if os.path.isfile(itempath):
+			total_size += os.path.getsize(itempath)
+		elif os.path.isdir(itempath):
+			total_size += getFolderSize(itempath)
+	return total_size
+
+def file_size(file_path):
+	"""
+	this function will return the file size
+	"""
+	if os.path.isfile(file_path):
+		file_info = os.stat(file_path)
+		return convert_bytes(file_info.st_size)
+
 def removedirtree(file_path):
 	for root, dirs, files in os.walk(file_path):
 		if root != file_path:
@@ -85,37 +126,40 @@ logger.addHandler(hdlr)
 logger.setLevel(logging.WARNING)
 
 size = 800, 800
-directory = "/volume1/Dati/Immagini/2016"
-dirthumb = "/volume1/photo"
-removedirtree(dirthumb)
+arraydir = [2015,2016]
+dirfrom = "/volume1/Dati/Immagini"
+dirto = "/volume1/photo"
+removedirtree(dirto)
 
 
-for root, dirs, files in os.walk(directory):
-	print"***********************************"
-	dimensione = convert_bytes(getFolderSize(root))
-	print root + " - " + dimensione
-	logger.info(root + " - " + dimensione)
-	newdir = root.replace(directory,dirthumb)
-	if root != directory:
+for direct in arraydir:
+	directory = "%s/%s"%(dirfrom ,direct)
+	dirthumb = "%s/%s"%(dirto ,direct)
+
+	for root, dirs, files in os.walk(directory):
+		print"***********************************"
+		dimensione = convert_bytes(getFolderSize(root))
+		print root + " - " + dimensione
+		logger.info(root + " - " + dimensione)
+		newdir = root.replace(directory,dirthumb)
 		print "mkdir: " + newdir
 		make_sure_path_exists(newdir)
-	numfile = 0
-	for filename in os.listdir(root):
-		if filename.lower().endswith(".jpg"):
-			try:
-				infile = os.path.join(root, filename)
-				im = Image.open(infile)
-				im.thumbnail(size)
-				im.save(newdir + "/" + filename, "JPEG")
-				print infile
-				print newdir + "/" + filename
-				numfile = numfile + 1
-			except:
-				messaggio = "errore di conversione al file %s" % infile
-				print messaggio
-				logger.error(messaggio)
-	print "convertiti %s file" % numfile
-	logger.info("convertiti %s file" % numfile)
-            
 
-            
+	sys.exit()
+		numfile = 0
+		for filename in os.listdir(root):
+			if filename.lower().endswith(".jpg"):
+				try:
+					infile = os.path.join(root, filename)
+					im = Image.open(infile)
+					im.thumbnail(size)
+					im.save(newdir + "/" + filename, "JPEG")
+					print infile
+					print newdir + "/" + filename
+					numfile = numfile + 1
+				except:
+					messaggio = "errore di conversione al file %s" % infile
+					print messaggio
+					logger.error(messaggio)
+		print "convertiti %s file" % numfile
+		logger.info("convertiti %s file" % numfile)
