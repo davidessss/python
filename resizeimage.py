a4cce70afb871042661c03b7a0007e95272b542c
davidessss <dvdgsxr@gmail.com>
2017-02-27 11:41:21 +0100

    Update resizeimage.py

--- resizeimage.py
+++ resizeimage.py
@@ -24,31 +24,57 @@ def convert_bytes(num):
 def getFolderSize(folder):
     total_size = os.path.getsize(folder)
     for item in os.listdir(folder):
-        itempath = os.path.join(folder, item)
-        if os.path.isfile(itempath):
-            total_size += os.path.getsize(itempath)
-        elif os.path.isdir(itempath):
-            total_size += getFolderSize(itempath)
-    return total_size
+from PIL import Image
+import os
+import errno
+import shutil
+import glob
+import sys
+import logging
+
+def make_sure_path_exists(path):
+	try:
+		os.makedirs(path)
+	except OSError as exception:
+		if exception.errno != errno.EEXIST:
+			raise
+def convert_bytes(num):
+	"""
+	this function will convert bytes to MB.... GB... etc
+	"""
+	for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+		if num < 1024.0:
+			return "%3.1f %s" % (num, x)
+		num /= 1024.0
+		
+def getFolderSize(folder):
+	total_size = os.path.getsize(folder)
+	for item in os.listdir(folder):
+		itempath = os.path.join(folder, item)
+		if os.path.isfile(itempath):
+			total_size += os.path.getsize(itempath)
+		elif os.path.isdir(itempath):
+			total_size += getFolderSize(itempath)
+	return total_size
 
 def file_size(file_path):
-    """
-    this function will return the file size
-    """
-    if os.path.isfile(file_path):
-        file_info = os.stat(file_path)
-        return convert_bytes(file_info.st_size)
+	"""
+	this function will return the file size
+	"""
+	if os.path.isfile(file_path):
+		file_info = os.stat(file_path)
+		return convert_bytes(file_info.st_size)
 
 def removedirtree(file_path):
-    for root, dirs, files in os.walk(file_path):
-        if root != file_path:
-            shutil.rmtree(root)
-            print "delete: " + root
-        else:
-            r = glob.glob(root+"/*.*")
-            for i in r:
-                os.remove(i)
-                print "delete file: " + i
+	for root, dirs, files in os.walk(file_path):
+		if root != file_path:
+			shutil.rmtree(root)
+			print "delete: " + root
+		else:
+			r = glob.glob(root+"/*.*")
+			for i in r:
+				os.remove(i)
+				print "delete file: " + i
 
 
 logger = logging.getLogger('resizeimage')
@@ -65,17 +91,17 @@ removedirtree(dirthumb)
 
 
 for root, dirs, files in os.walk(directory):
-    print"***********************************"
+	print"***********************************"
 	dimensione = convert_bytes(getFolderSize(root))
-    print root + " - " + dimensione
+	print root + " - " + dimensione
 	logger.info(root + " - " + dimensione)
-    newdir = root.replace(directory,dirthumb)
-    if root != directory:
-        print "mkdir: " + newdir
-        make_sure_path_exists(newdir)
-    numfile = 0
-    for filename in os.listdir(root):
-        if filename.lower().endswith(".jpg"):
+	newdir = root.replace(directory,dirthumb)
+	if root != directory:
+		print "mkdir: " + newdir
+		make_sure_path_exists(newdir)
+	numfile = 0
+	for filename in os.listdir(root):
+		if filename.lower().endswith(".jpg"):
 			try:
 				infile = os.path.join(root, filename)
 				im = Image.open(infile)
@@ -88,7 +114,7 @@ for root, dirs, files in os.walk(directory):
 				messaggio = "errore di conversione al file %s" % infile
 				print messaggio
 				logger.error(messaggio)
-    print "convertiti %s file" % numfile
+	print "convertiti %s file" % numfile
 	logger.info("convertiti %s file" % numfile)
             
 
